441a3306ab8883a8e02aa1dfd4e5a21a005bf954
ConnorC04 <connorjcasey@iCloud.com>
2025-02-27 13:28:53 -0500

    Done with jshell and JavaSmall

--- sumNumbers.java
+++ sumNumbers.java
@@ -1,6 +1,3 @@
-sumNumbers(5);
-sumNumbers(10);
-sumNumbers(106839);
 int sumNumbers(int n){
 	int sum = 0;
 	for (int i = 0; i <= n; i++){
@@ -28,3 +25,39 @@ int multiplyTwoNumber(int x, int y){
 int subtractTwoNumber(int x, int y){
 	return x - y;
 }
+boolean isEven(int x){
+	if (x % 2 == 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+isEven(5);
+isEven(8);
+boolean isOdd(int x){
+	if (x % 2 == 0){
+		return false;
+	}else{
+		return true;
+	}
+}
+isOdd(3);
+isOdd(2);
+int i = 1;
+while (i <= 10){
+	System.out.println(i + " % 2 = " + i % 2);
+	i++;
+}
+void printOddEven(){
+	int i = 1;
+	while (i <= 10){
+		if (isEven(i)){
+			System.out.println(i + " is even.");
+		}else{
+			System.out.println(i + " is odd.");
+		}
+		i++;
+	}
+}
+	
+printOddEven();
