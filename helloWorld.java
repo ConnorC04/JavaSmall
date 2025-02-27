16fa1556f627d5e36821ba15cbf128e5222d6adf
ConnorC04 <connorjcasey@iCloud.com>
2025-02-27 12:44:05 -0500

    Startup Jshell

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+System.out.println("Hello World");
+int x = 5;
+x
+x + 5
+x = x + 10
+x
+void println(String s){System.out.println(s);}
+println("your name here!");
+int i = 1;
+while (i<=10){
+    System.out.println(i);
+    i++;}
